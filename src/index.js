3c9167914da74ce1cc5636d47dbb414065c9879a
Jay Wigard <jayw@autowares.com>
2016-10-26 11:19:29 -0400

    use cross-spawn bcuz duh

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 
 var Git = require('./git');
 
-var ChildProcess = require('child_process');
+//var ChildProcess = require('child_process');
+var CrossSpawn = {spawn: require('cross-spawn')};
 var Buffer = require('buffer').Buffer;
 
 module.exports = function (baseDir) {
-    return new Git(baseDir || process.cwd(), ChildProcess, Buffer);
+    return new Git(baseDir || process.cwd(), CrossSpawn, Buffer);
 };
 
